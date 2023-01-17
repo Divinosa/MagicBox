5f3d502cd261122b253e6b5d11968c9d40f4075d
Danila EV <dalnas@mail.ru>
2023-01-17 17:12:04 +0300

    add check

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -23,12 +23,22 @@ public class MagicBox<T>  {
     public T pick (){
         Random random = new Random();
         int randomInt = random.nextInt(this.item.length - 1);
-        if (this.item[randomInt] != null){
+        if (this.check() == 0){
             return this.item[randomInt];
         } else {
-            throw new RuntimeException("пустая коробка");
+            throw new RuntimeException("кол-во свободных ячеек: " + this.check());
         }
 
+    }
+
+    public int check(){
+        int check = 0;
+        for (int i = 0; i < this.item.length; i++){
+            if (this.item[i] == null){
+                check += 1;
+            }
+        }
+        return check;
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,11 @@ public class Main {
     public static void main(String[] args) {
         String[] names = new String[3];
         MagicBox <String> magicBoxNames = new MagicBox<String>(names);
-        Integer[] years = new Integer[3];
-        MagicBox <Integer> magicBoxYears = new MagicBox<Integer>(years);
 
-        //magicBoxNames.add("Вася");
-        //magicBoxNames.add("Петя");
-        //magicBoxNames.add("Саша");
+
+        magicBoxNames.add("Вася");
+        magicBoxNames.add("Петя");
         System.out.println(magicBoxNames.pick());
-
 
 
     }
